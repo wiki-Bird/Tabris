3816af7068fcd72f48518c6c62ad56cdc3738c50
KwiiHours <41028518+KwiiHours@users.noreply.github.com>
2023-01-23 16:57:31 +1300

    Added app

---
+++ main.js
@@ -0,0 +1,44 @@
+var button = document.querySelector('.downloadButton');
+var url = document.querySelector('.inputBox');
+var error = document.querySelector('.error');
+let server = "http://localhost:4000";
+
+button.addEventListener('click', function() {
+    if (!url.value) {
+        console.log("no url given")
+        error.innerHTML = "No URL given";
+        error.style.display = "block";
+        setTimeout(function() {
+            error.style.display = "none";
+        }, 2000);
+    }
+    else {
+        downloadVid(url.value);
+    }
+});
+
+async function downloadVid(url){
+    error.innerHTML = "Downloading...";
+    error.style.display = "block";
+    const res = await fetch(`${server}/downloadVid?url=${url}`);
+
+	if(res.status == 200) {
+		var a = document.createElement('a');
+  		a.href = `${server}/downloadVid?url=${url}`;
+  		a.setAttribute('download', '');
+		a.click();
+
+        error.innerHTML = "Downloaded";
+        setTimeout(function() {
+            error.style.display = "none";
+        }, 2000);
+	} 
+    else if(res.status == 400) {
+        console.log("Invalid URL");
+        error.innerHTML = "Invalid URL";
+        error.style.display = "block";
+        setTimeout(function() {
+            error.style.display = "none";
+        }, 2000);
+	}
+}
---
+++ server/index.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const cors = require('cors');
+const ytdl = require('ytdl-core');
+
+const app = express();
+app.use(cors());
+
+app.listen(4000, () => {
+    console.log('Server up and running on port 4000');
+});
+
+app.get("/downloadVid", async (req, res) => {
+    var url = req.query.url;
+    var title = "video";
+    
+    if (!ytdl.validateURL(url)) {
+        return res.sendStatus(400);
+    }
+
+    let info = await ytdl.getInfo(url);
+    title = info.player_response.videoDetails.title;
+
+    let format = ytdl.chooseFormat(info.formats, {
+        quality: 'highest',
+        filter: 'audioandvideo'
+    });
+    if (!format) { 
+        return res.status(400).send("Couldn't find good format"); 
+    }
+
+    res.setHeader('Content-disposition', `attachment; filename=${title}.${format.container}`);
+    res.setHeader('Content-Type', `video/${format.container}`);
+    ytdl(url, { format: format }).pipe(res);
+});
